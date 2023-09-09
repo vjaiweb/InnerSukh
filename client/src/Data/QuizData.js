5b0df785136faa9ea2c17e35ddfcc7b1c5513fa6
pranjaljain2882 <jain.pranjal12345@gmail.com>
2023-09-09 20:58:27 +0530

    Quiz

---
+++ client/src/Data/QuizData.js
@@ -0,0 +1,96 @@
+const questions = [
+    {
+      text: "Over the last two weeks, how often have you felt little interest or pleasure in doing things you usually enjoy?",
+      options: [
+        { id: 0, opt: "a", text: "Not at all" },
+        { id: 1, opt: "b", text: "Several days"  },
+        { id: 2, opt: "c", text: "More than half the days" },
+        { id: 3, opt: "d", text: "Nearly every day" }
+      ]
+    },
+    {
+      text: "Over the last two weeks, how often have you felt down, depressed, or hopeless?",
+      options: [
+        { id: 0, opt: "a", text: "Not at all" },
+        { id: 1, opt: "b", text: "Several days"  },
+        { id: 2, opt: "c", text: "More than half the days" },
+        { id: 3, opt: "d", text: "Nearly every day" }
+      ]
+    },
+    {
+      text: "Have you experienced significant changes in your appetite or weight (either loss or gain) recently?",
+      options: [
+        { id: 0, opt: "a", text: "No changes"},
+        { id: 1, opt: "b", text: "A little change"},
+        { id: 2, opt: "c", text: "Moderate change" },
+        { id: 3, opt: "d", text: "Significant change"}
+      ]
+    },
+    {
+      text: "How has your sleep pattern been affected? Have you experienced insomnia or excessive sleeping?",
+      options: [
+        { id: 0, opt: "a", text: "No change" },
+        { id: 1, opt: "b", text: "Occasional trouble sleeping"  },
+        { id: 2, opt: "c", text: "Frequent insomnia or excessive sleep" },
+        { id: 3, opt: "d", text: "Severe insomnia or excessive sleep" }
+      ]
+    },
+    {
+      text: "Are you frequently fatigued or lacking energy?",
+      options: [
+        { id: 0, opt: "a", text: "Rarely" },
+        { id: 1, opt: "b", text: "Sometimes"  },
+        { id: 2, opt: "c", text: "Often" },
+        { id: 3, opt: "d", text: "Constantly" }
+      ]
+    },
+    {
+      text: "Have you noticed a decrease in your ability to concentrate or make decisions?",
+      options: [
+        { id: 0, opt: "a", text: "No" },
+        { id: 1, opt: "b", text: "Occasionally"  },
+        { id: 2, opt: "c", text: "Frequently" },
+        { id: 3, opt: "d", text: "Constantly" }
+      ]
+    },
+    {
+      text: "Have you felt excessively guilty or worthless?",
+      options: [
+        { id: 0, opt: "a", text: "No" },
+        { id: 1, opt: "b", text: "Rarely"  },
+        { id: 2, opt: "c", text: "Often" },
+        { id: 3, opt: "d", text: "Constantly" }
+      ]
+    },
+
+    {
+      text: "Have you noticed changes in your daily activities, such as withdrawing from social interactions or not engaging in hobbies you once enjoyed?",
+      options: [
+        { id: 0, opt: "a", text: "No change" },
+        { id: 1, opt: "b", text: "Slight decrease"  },
+        { id: 2, opt: "c", text: "Moderate decrease" },
+        { id: 3, opt: "d", text: "Severe decrease" }
+      ]
+    },
+    {
+      text: "Have you experienced changes in your weight or appetite? If so, have you been eating more or less than usual?",
+      options: [
+        { id: 0, opt: "a", text: "No change" },
+        { id: 1, opt: "b", text: "Slight decrease"  },
+        { id: 2, opt: "c", text: "Moderate decrease" },
+        { id: 3, opt: "d", text: "Significant changes" }
+      ]
+    },
+    {
+      text: "Have you noticed a decrease in your ability to concentrate on tasks, such as work or school assignments?",
+      options: [
+        { id: 0, opt: "a", text: "No" },
+        { id: 1, opt: "b", text: "Occasionally"  },
+        { id: 2, opt: "c", text: "Frequently" },
+        { id: 3, opt: "d", text: "Constantly" }
+      ]
+    },
+
+  ];
+
+export default questions;  
