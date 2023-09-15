843427a429b8a1916d6a149ecde4d6cb61422c18
vaibhav-gupta09 <vaibhavgupta031209@gmail.com>
2023-09-15 10:18:16 +0530

    pause audio on back and gratitude setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import { Quiz } from "./Components/Quiz/Quiz.js";
 import Drop from "./pages/21days/21day";
 import Meditation from "./pages/meditation/Meditation";
+import Gratitude from "./pages/gratitude/Gratitude";
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Drop/>}/>
           <Route path="/meditation" element={<Meditation/>}/>
+          <Route path="/gratitude" element={<Gratitude/>}/>
         </Routes>
          
          {/* <Quiz /> */}
--- client/src/pages/gratitude/Gratitude.jsx
+++ client/src/pages/gratitude/Gratitude.jsx
@@ -1,19 +1,16 @@
 import React, { useEffect, useState } from "react";
 
-import "./Meditation.css";
-import meditationAudio from "../../Assets/Audio/meditationAudio.mp3";
+import "./Gratitude.css";
+import gratitudeAudio from "../../Assets/Audio/gratitude.mp3";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
-
-
-const Meditation = () => {
+const Gratitude = () => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const navigate = useNavigate()
-  const audioElement = new Audio(meditationAudio);  
-  const [audioDuration, setAudioDuration] = useState(0)
-
+  const navigate = useNavigate();
+  const audioElement = new Audio(gratitudeAudio);
+  const [audioDuration, setAudioDuration] = useState(0);
 
   const playAudio = () => {
     audioElement.play();
@@ -27,7 +24,6 @@ const Meditation = () => {
     const timer = setTimeout(() => {
       stopAudioAndNavigate();
     }, audioDuration); // 60,000 milliseconds = 1 minute
-
   };
 
   const stopAudioAndNavigate = () => {
@@ -42,43 +38,39 @@ const Meditation = () => {
     setIsPlaying(false);
     navigate("/");
     window.location.reload();
-  }
+  };
 
   const handleBeforeUnload = (e) => {
     if (isPlaying) {
       // Display a confirmation message if audio is playing
       e.preventDefault();
-      
-      const message =  "Your meditation session will start from the beginning if you refresh the page. Are you sure you want to continue?";
+
+      const message =
+        "Your meditation session will start from the beginning if you refresh the page. Are you sure you want to continue?";
       e.returnValue = message;
     }
   };
-  
-
 
   useEffect(() => {
     audioElement.addEventListener("loadedmetadata", () => {
-      setAudioDuration((audioElement.duration) * 1000); // Convert to milliseconds
+      setAudioDuration(audioElement.duration * 1000); // Convert to milliseconds
     });
-    
-
 
     window.addEventListener("beforeunload", handleBeforeUnload);
-     window.addEventListener("popstate", () => {
-       if(isPlaying){
-          handleBackBtnClick();
-       }
-     });
+    window.addEventListener("popstate", () => {
+      if (isPlaying) {
+        handleBackBtnClick();
+      }
+    });
 
     // Clean up the event listener when the component unmounts
-    return () => {  
+    return () => {
       window.removeEventListener("beforeunload", handleBeforeUnload);
-
     };
   }, [isPlaying]);
 
   return (
-    <div className="meditation-container">
+    <div className="gratitude-container">
       <button
         className={`transparent-button ${isPlaying ? "hidden" : ""}`}
         onClick={playAudio}
@@ -99,4 +91,4 @@ const Meditation = () => {
   );
 };
 
-export default Meditation;
+export default Gratitude;
